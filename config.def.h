caab4a4cb37f692bcb177bd6fa14fe7f0fb5bc3f
Erfan Mirshams <erfanmirshams@gmail.com>
2022-07-09 14:46:16 +0430

    fix the issue with background color

--- config.def.h
+++ config.def.h
@@ -133,8 +133,8 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
--- config.h
+++ config.h
@@ -123,7 +123,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"black",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 };
 
 
@@ -132,8 +133,8 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
