e87f6185e688fd3ace9cedc3a02bb1c1fe70e3df
Erfan Mirshams <erfanmirshams@protonmail.com>
2023-04-10 21:23:41 +0330

    update font size for HighDPI display

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejavuSansMono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 15;
+static char *font = "DejavuSansMono:pixelsize=25:antialias=true:autohint=true";
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
